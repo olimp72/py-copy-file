4b7f9b7af38b8e871b9df834606f56248db5bcf3
Andrey Bilenko <olimp_72@ukr.net>
2026-01-05 00:19:13 +0200

    Fix_Bags_Solution

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 def copy_file(command: str) -> None:
     parts = command.split()
-    if len(parts) < 3 or parts[0] != "cp":
+    if len(parts) != 3 or parts[0] != "cp":
         return
     source = parts[1]
     destination = parts[2]
